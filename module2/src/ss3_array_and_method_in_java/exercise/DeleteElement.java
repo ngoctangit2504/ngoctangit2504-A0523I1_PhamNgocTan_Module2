9d70ac0c0714b8354ce26f998f7df3935cfbb5bc
ngoctangit2504 <lulu25042001@gmail.com>
2023-08-10 23:57:14 +0700

    ss3 delete element

---
+++ module2/src/ss3_array_and_method_in_java/exercise/DeleteElement.java
@@ -0,0 +1,25 @@
+package ss3_array_and_method_in_java.exercise;
+
+import java.util.Scanner;
+
+public class DeleteElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = {1,5,6,8,9};
+        System.out.println("Nhập phần tử cần xóa");
+        int x = scanner.nextInt();
+        int index_del = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (x == arr[i]){
+                index_del = i;
+                System.out.println("X có tồn tại trong mảng tại vị trí "+index_del);
+            }
+        }
+        for (int j = index_del; j < arr.length-1; j++) {
+            arr[j] = arr[j+1];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
