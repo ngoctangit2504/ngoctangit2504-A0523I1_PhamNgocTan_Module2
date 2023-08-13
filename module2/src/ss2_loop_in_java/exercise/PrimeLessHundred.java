bdef9798ff0ffb77cadd3d15c4575b08395f7535
ngoctangit2504 <lulu25042001@gmail.com>
2023-08-13 14:01:39 +0700

    ss2 update

--- module2/src/ss2_loop_in_java/exercise/PrimeLessHundred.java
+++ module2/src/ss2_loop_in_java/exercise/PrimeLessHundred.java
@@ -3,23 +3,23 @@ package ss2_loop_in_java.exercise;
 public class PrimeLessHundred {
     public static void main(String[] args) {
         System.out.println("---Hiển thị 100 số nguyên tố đầu tiên---");
-        int N = 2;
-        System.out.println(N + "\n");
+        int n = 2;
+        System.out.println(n + "\n");
 
         for (int count = 0; count < 100; ) {
             boolean flag = true;
-            if (N > 2) {
-                for (int j = 2; j < N; j++) {
-                    if (N % j == 0) {
+            if (n > 2) {
+                for (int j = 2; j < n; j++) {
+                    if (n % j == 0) {
                         flag = false;
                     }
                 }
                 if (flag) {
-                    System.out.println(N + ",");
+                    System.out.println(n + ",");
                     count++;
                 }
             }
-            N++;
+            n++;
         }
     }
 }
