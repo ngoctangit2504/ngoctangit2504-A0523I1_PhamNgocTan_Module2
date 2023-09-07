1eb2c38b123af2a480a19990b96d128fe02d895a
ngoctangit2504 <lulu25042001@gmail.com>
2023-09-07 19:50:51 +0700

    ss10 update

---
+++ module2/src/ss10_danh_sach/exercise/arraylist_nomal/MyList.java
@@ -0,0 +1,13 @@
+package ss10_danh_sach.exercise.arraylist_nomal;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {}
+}
---
+++ module2/src/ss10_danh_sach/exercise/arraylist_nomal/MyListTest.java
@@ -0,0 +1,7 @@
+package ss10_danh_sach.exercise.arraylist_nomal;
+
+public class MyListTest {
+    public static void main(String[] args) {
+
+    }
+}
