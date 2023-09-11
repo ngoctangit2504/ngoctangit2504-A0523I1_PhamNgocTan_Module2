d103dc7dd7ec27fbbea666609200ba5471b3e1e4
ngoctangit2504 <lulu25042001@gmail.com>
2023-09-11 11:41:29 +0700

    ss10 exercise array list

--- module2/src/ss10_danh_sach/exercise/arraylist_nomal/MyList.java
+++ module2/src/ss10_danh_sach/exercise/arraylist_nomal/MyList.java
@@ -1,13 +1,120 @@
 package ss10_danh_sach.exercise.arraylist_nomal;
 
+import java.util.Arrays;
+
 public class MyList<E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
 
     public MyList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        elements = (Object[]) new Object[DEFAULT_CAPACITY];
+    };
+
+    public MyList(int capacity) {
+        if (capacity >= 0) {
+            elements = (Object[]) new Object[capacity];
+        } else {
+            throw new IllegalArgumentException(String.valueOf(capacity));
+        }
     }
 
-    public MyList(int capacity) {}
+    public void add(int index, E element) {
+        if (index > elements.length) {
+            throw new IllegalArgumentException(String.valueOf(index));
+        } else if (elements.length == size) {
+            this.ensureCapacity(5);
+        } if (elements[index] == null) {
+            elements[index] = element;
+            size++;
+        } else {
+            for (int i = 0; i < elements.length; i++) {
+              elements[i] = null;
+            }
+        }
+        // oke;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > elements.length) {
+            throw new IllegalArgumentException(String.valueOf(index));
+        }
+        E element = (E) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i +1];
+        }
+        elements[size - 1] = null;
+        size--;
+        return element;
+        // oke;
+    }
+
+    public int size(){
+        return size;
+        // oke;
+    }
+
+//    public E clone(){
+//        MyList<E> e = new MyList<>();
+//        e.elements = Arrays.copyOf(elements,size);
+//        e.size = this.size;
+//        return (E) e;
+//    }
+
+    public boolean contains(E o){
+        return indexOf(o) >= 0;
+        // oke;
+    }
+
+    public int indexOf(E o){
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (this.elements[i].equals(o)){
+                return i;
+            }
+        }
+        return index;
+        // oke;
+    }
+
+    public boolean add(E element){
+        if (elements.length == size){
+            this.ensureCapacity(3);
+        }
+        elements[size] = elements;
+        size++;
+        return true;
+        // oke;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity >= 0) {
+            int newSize = this.elements.length + minCapacity;
+            elements = Arrays.copyOf(elements,newSize);
+        } else {
+            throw new IllegalArgumentException(String.valueOf(minCapacity));
+        }
+        // oke;
+    }
+
+    public E get(int i){
+        return (E) elements[i];
+        // oke;
+    }
+
+    public void clear(){
+        size = 0;
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+        // oke;
+    }
+
+    @Override
+    public String toString() {
+        return "MyList{" +
+                "size=" + size +
+                ", elements=" + Arrays.toString(elements) +
+                '}';
+    }
 }
--- module2/src/ss10_danh_sach/exercise/arraylist_nomal/MyListTest.java
+++ module2/src/ss10_danh_sach/exercise/arraylist_nomal/MyListTest.java
@@ -2,6 +2,12 @@ package ss10_danh_sach.exercise.arraylist_nomal;
 
 public class MyListTest {
     public static void main(String[] args) {
+        MyList<String> list = new MyList<>();
+        list.add(0,"pro");
+        list.add(1,"vip");
+        list.add(2,"alo");
+        list.add(5,"alo");
 
+        System.out.println(list);
     }
 }
